7bd005d26ebe5cc588c7374e7c286a2bd6f75d8f
ec22409 <ec22409@qmul.ac.uk>
2024-01-28 20:00:47 +0000

    Test Start

---
+++ src/GridCreator.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class GridCreator {
+    public static void gridInsert(){
+        System.out.println("happy");
+    }
+}
