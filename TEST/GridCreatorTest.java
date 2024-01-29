b3652eecb41205c36f989eeb511ae457289e1685
ec22409 <ec22409@qmul.ac.uk>
2024-01-29 00:31:48 +0000

    Test Start

--- TEST/GridCreatorTest.java
+++ TEST/GridCreatorTest.java
@@ -1,10 +1,26 @@
-import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.stream.Stream;
 
 class GridCreatorTest {
 
-    @Test
-    void gridInsert() {
+    @ParameterizedTest
+    @MethodSource("gridInsertTestVals")
+       //     "100,002,004",
+        //    "002,000,000",
+         //   "100,234,000"
+    void gridInsert(String row1, String row2, String row3) {
+        GridCreator gc = new GridCreator();
+        String[] grid = gc.gridInsert(row1,row2,row3);
+        Assert.assertArrayEquals();
+    }
+    private static Stream<Arguments> gridInsertTestVals(){
+        return Stream.of(
+                Arguments.of("100","002","004",new String[][]{})
+        )
     }
 }
--- src/GridCreator.java
+++ src/GridCreator.java
@@ -3,7 +3,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class GridCreator {
-    public static void gridInsert(){
-        System.out.println("happy");
+    public  String[] gridInsert(String row1, String row2,String row3){
     }
 }
