6e4b9fcea5236599de1b72e97bbc879bcd81c2ee
aivarasz <barcysa@gmail.com>
2024-02-07 10:26:16 +0000

    Duplicate Checking code Complete!

--- src/GridCreator.java
+++ src/GridCreator.java
@@ -1,16 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class GridCreator {
     String[] grid;
 
+    //Use array list so that i can search and remove values with ease
+    ArrayList<Character> available_digits;
     public GridCreator() {
         this.grid = new String[]{};
+        this.available_digits = new ArrayList<>(Arrays.asList('1','2','3','4','5','6','7','8','9'));
     }
 
     //This method should only accept 3 sets of numbers of length 3.
     public void gridInsert(String row1, String row2, String row3){
         if (row1.length() == 3 && row2.length() == 3 && row3.length() == 3){
-            if(numCheck(row1,row2,row3)){
+            if(numCheck(row1,row2,row3) && !(this.duplicateCheck(row1,row2,row3))){
                 this.grid = new String[]{row1, row2, row3};
                 return;
             }
@@ -52,10 +58,22 @@ public class GridCreator {
             temp = String.valueOf(this.grid[i].charAt(2));
             row = row +"[" + temp + "]";
             System.out.println(row);
-
         }
     }
+    //Return TRUE if there is a shared number between the 3 rows. 0 not included.
     public boolean duplicateCheck(String row1,String row2, String row3){
+        String grid = row1+row2+row3;
+        for(int i=0;i<9;i++){
+            char digit = grid.charAt(i);
+            if(digit == '0'){}
+            else if((this.available_digits.contains(digit))){
+                //A digit is removed as it is no longer an available choice
+                this.available_digits.remove(Character.valueOf(digit));
+            }
+            else{ //This means there is a duplicate is found
+                return true;
+            }
+        }
         return false;
     }
 
