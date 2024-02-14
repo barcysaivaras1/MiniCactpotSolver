6da08bc87dd6c98e12e6acf2afb006aeca4b3f1f
ec22409 <ec22409@qmul.ac.uk>
2024-02-14 18:41:56 +0000

    Refactored Driver, now prints out average of each line

--- src/CactpotDriver.java
+++ src/CactpotDriver.java
@@ -1,7 +1,23 @@
+import java.util.Map;
+
 public class CactpotDriver {
     public static void main(String[] args) {
         Grid gc = new Grid();
         gc.inputRows();
         gc.printGrid();
+        Map<String,Integer> LINE_SCORES = gc.MapsTheAverageScoreForEachLine();
+        int HIGHEST_VAL = 0;
+        String HIGHEST_VAL_POSITION = "";
+        for(Map.Entry<String,Integer> entry : LINE_SCORES.entrySet()){
+            String key = entry.getKey();
+            System.out.println(entry.getKey()+" = "+entry.getValue());
+            if (entry.getValue()>HIGHEST_VAL){
+                HIGHEST_VAL = entry.getValue();
+                HIGHEST_VAL_POSITION = entry.getKey();
+            }
+        }
+        System.out.println("Reveal a position in "+HIGHEST_VAL_POSITION);
     }
+
+    //Create more code to allow user to input what value was revealed when they made the suggested choice. This should print out new grid and averages
 }
